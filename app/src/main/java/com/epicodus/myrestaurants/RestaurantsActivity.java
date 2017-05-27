afb77634a71ee8003f801ceaa1bce7cc37f1f422
Xi Xia <triplextension@gmail.com>
2017-05-27 00:37:50 -0700

    first yelp made

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -2,18 +2,21 @@ package com.epicodus.myrestaurants;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends Activity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
@@ -31,7 +34,6 @@ public class RestaurantsActivity extends Activity {
         setContentView(R.layout.activity_restaurants);
 
         ButterKnife.bind(this);
-
         MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines);
         mListView.setAdapter(adapter);
 
@@ -40,15 +42,31 @@ public class RestaurantsActivity extends Activity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String restaurant = ((TextView)view).getText().toString();
                 Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-                    Log.v("T", "onItemClickListener!");
-
                 }
         });
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        getRestaurants(location);
         mLocationTextView.setText("Here are all the restaurants near: " + location);
-        Log.d("RestaurantsActivity", "In the onCreate method!");
+    }
 
+    private void getRestaurants (String location){
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+        @Override
+            public void onFailure(Call call, IOException e){
+            e.printStackTrace();
+        }
+        @Override
+            public void onResponse (Call call, Response response) throws IOException {
+            try {
+                String jsonData = response.body().string();
+                Log.v(TAG, jsonData);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        });
     }
 }
