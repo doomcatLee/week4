a2a6cf5b1bfce814ec64f4ec80991bb185518ce3
Arlen Burton and Xi Xia <arlen@>
2017-06-05 10:12:33 -0700

    added firebase save and delete

---
+++ app/src/main/java/com/epicodus/myrestaurants/SavedRestaurantListActivity.java
@@ -0,0 +1,12 @@
+package com.epicodus.myrestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedRestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -0,0 +1,8 @@
+package com.epicodus.myrestaurants.adapters;
+
+/**
+ * Created by Guest on 6/5/17.
+ */
+
+public class FirebaseRestaurantViewHolder {
+}
